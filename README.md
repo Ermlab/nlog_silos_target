3db5342f09a6d87ddecdd64862fabdbe9edf9393
westsajd <westsajd@gmail.com>
2014-09-03 11:51:59 +0200

    readme, sample config and SilosTarget project

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ nlog_silos_target
 =================
 
 NLog target for silos logging service
+
+Usage:
+1. First we need to add reference to SilosWrapper project (or .dll file) to a project where we want to do some logging.
+2. Run Package Manager Console in logging project and type (or download via NuGet):
+	install-package NLog
+	install-package NLog.Config (to download a sample config file for NLog)
+3. In NLog.config set your configuration for logging (sample file included).
+4. Use logger in your code:
+	NLog.LogManager.GetCurrentClassLogger().Info("Sample log.");
+	
---
+++ SilosTarget/SiloWrapper/SilosTarget.cs
@@ -0,0 +1,121 @@
+﻿using NLog;
+using NLog.Config;
+using NLog.Targets;
+using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+
+namespace SilosWrapper
+{
+    [Target("Silos")]
+    public class SilosTarget : TargetWithLayout
+    {
+        private readonly Uri host = new Uri("http://silos.ermlab.com/api/call/");
+
+        public SilosTarget()
+        {
+        }
+
+        private string apiKey;
+
+        [RequiredParameter]
+        public string ApiKey
+        {
+            get
+            {
+                return apiKey;
+            }
+            set
+            {
+                apiKey = value;
+            }
+        }
+
+        private string logbookName;
+
+        [RequiredParameter]
+        public string LogbookName
+        {
+            get
+            {
+                return logbookName;
+            }
+            set
+            {
+                logbookName = value;
+            }
+        }
+
+        private string logger;
+
+        public string Logger
+        {
+            get
+            {
+                return logger;
+            }
+            set
+            {
+                logger = value;
+            }
+        }
+
+
+        protected override void Write(LogEventInfo logEvent)
+        {
+            string logMessage = this.Layout.Render(logEvent);
+
+            using (var client = new WebClient())
+            {
+                var values = new NameValueCollection();
+                values["name"] = this.LogbookName;
+                values["logger"] = this.Logger;
+                values["body"] = logMessage;
+                values["level"] = this.findLevel(logEvent);
+
+                var response = client.UploadValues(combineHostWithKey(), values);
+
+                var responseString = Encoding.Default.GetString(response);
+            }
+        }
+
+        private string combineHostWithKey()
+        {
+            return string.Format("{0}{1}", this.host, this.ApiKey);
+        }
+
+        private string findLevel(LogEventInfo logEvent)
+        {
+            //default level is debug
+            int result = 2;
+
+            if (logEvent.Level == LogLevel.Trace)
+            {
+                result = 1;
+            }
+            else if (logEvent.Level == LogLevel.Debug)
+            {
+                result = 2;
+            }
+            else if (logEvent.Level == LogLevel.Info)
+            {
+                result = 3;
+            }
+            else if (logEvent.Level == LogLevel.Warn)
+            {
+                result = 4;
+            }
+            else if (logEvent.Level == LogLevel.Error)
+            {
+                result = 5;
+            }
+            else if (logEvent.Level == LogLevel.Fatal)
+            {
+                result = 6;
+            }
+
+            return result.ToString();
+        }
+    }
+}
